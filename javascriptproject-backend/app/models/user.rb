1f071f11b72d1e0173ecacf7d4e9e54575b45522
Christian_Polanco <chrispolanco@sbcglobal.net>
2020-04-10 00:12:46 -0700

    validates added to user

--- javascriptproject-backend/app/models/user.rb
+++ javascriptproject-backend/app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has many :adventures
     has_many :heroes, through: :adventures
     accepts_nested_attributes_for :adventures
+
+    validates :name, :username, :password, presence: true 
 end
