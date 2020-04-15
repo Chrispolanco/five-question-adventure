53846f6c420ea7a5dc68e04fc317e406067fae5b
Christian_Polanco <chrispolanco@sbcglobal.net>
2020-04-14 22:33:40 -0700

    added json to hero

--- javascriptproject-backend/app/controllers/heros_controller.rb
+++ javascriptproject-backend/app/controllers/heros_controller.rb
@@ -1,2 +1,8 @@
 class HerosController < ApplicationController
+    
+    def index
+        heros = Hero.all 
+        render json: HeroSerializer.new(@heroes).to_serialized_json, status: 200
+    end 
+
 end
