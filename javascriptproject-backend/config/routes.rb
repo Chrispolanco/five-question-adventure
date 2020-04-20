20b675075342ce9be7738add0dd1b716015f028d
Christian_Polanco <chrispolanco@sbcglobal.net>
2020-04-19 22:43:31 -0700

    updated routes

--- javascriptproject-backend/config/routes.rb
+++ javascriptproject-backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :heros
-  resources :adventures
-  resources :users 
+  resources :heros, only: [:index, :new, :create, :show, :edit, :update]
+  resources :adventures, only: [:index, :new, :create, :show, :edit, :update]
+  resources :users , only: [:index, :new, :create, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/test', to: 'application#test'
 
