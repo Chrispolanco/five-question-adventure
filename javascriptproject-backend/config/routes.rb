d7877b08dc1ece52bd5e4671570779e7c0f81a5c
Christian_Polanco <chrispolanco@sbcglobal.net>
2020-04-21 22:51:35 -0700

    updated routes with sessions

--- javascriptproject-backend/config/routes.rb
+++ javascriptproject-backend/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :heros, only: [:index, :new, :create, :show, :edit, :update]
   resources :adventures, only: [:index, :new, :create, :show, :edit, :update]
   resources :users , only: [:index, :new, :create, :show, :edit, :update]
+  resources :sessions , only: [:index, :new, :create, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/test', to: 'application#test'
 
