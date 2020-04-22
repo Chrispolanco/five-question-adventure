7bd19c4b1c49df2079688471ecf974a4b33a65ad
Christian_Polanco <chrispolanco@sbcglobal.net>
2020-04-21 22:10:53 -0700

    adjusted serializer for user

--- javascriptproject-backend/app/serializers/user_serializer.rb
+++ javascriptproject-backend/app/serializers/user_serializer.rb
@@ -1,6 +1,14 @@
 class UserSerializer
-  include FastJsonapi::ObjectSerializer
-  attributes :username, :password_digest, :name
+
+  def initialize(user_object) 
+    @user = user_object
+  end 
+
+  def to_serialized_json
+    options = {
+      only: [:username, :password_digest, :name]
+    }
+    @user.to_json(options)
+  end 
+
 end
-
-
