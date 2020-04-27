3acd4c7c978fef798a6800a26788ece58e48b316
Christian_Polanco <chrispolanco@sbcglobal.net>
2020-04-26 21:57:35 -0700

    added newUser in adapter

--- javascriptproject-frontend/src/adapters/usersAdapter.js
+++ javascriptproject-frontend/src/adapters/usersAdapter.js
@@ -9,4 +9,22 @@ class UsersAdapter {
         return fetch(this.baseUrl).then(res => res.json())
     }
 
+    newUser(username, password_digest, name) {
+        const user = {
+            username: username, 
+            password_digest: password_digest, 
+            name: name
+        }
+        return fetch(this.base.URL, {
+            method: "POST", 
+            headers: {
+                "Content-Type": "application/json", 
+                "Accept": "application/json"
+            }, 
+            body: JSON.stringify(user)
+        })
+            .then(resp = resp.json())
+    }
+
 }
+
